9aa442a0d32aedea019aa4a221758ffd8e618e5a
HannaPolishchuk <hanna.polishchuk@gmail.com>
2025-11-10 19:41:30 +0100

    ADD: 3 evetListeners: submit, change and click.

--- js/main.js
+++ js/main.js
@@ -2,108 +2,146 @@
 document.addEventListener('DOMContentLoaded', () => {
     //VARIABLES
     const inputBuscador = document.querySelector('#buscador');
-    //console.log(inputBuscador);
     const buscadorFotos = document.querySelector('#buscadorFotos');
-    //console.log(formulario);
-    const botonBuscar = document.querySelector('#botonBuscar');//botón submit
-    //selector y sus opciones
-    //los 3 articles de categorias de imagenes: hay que darles un id en html!!
-    const mainContainer = document.querySelector('mainContainer'); //para añadir p de error cuando la palabra no es válida
+    const pError = document.querySelector('#pError');
+    const orientacion = document.querySelector('#orientacion');
+    const urlBase = `https://api.pexels.com/v1/`;
+    const autorizacion = `851Ebzs3BlLqHHT4VZBNwGS0F7vmu9UH97VyAfhj9mjWBNZ4FRA4zrjt`; // key de Sonia
+    const errorDOM = document.querySelector('#errorDOM');
     const containerGaleria = document.querySelector('#containerGaleria');
-    const pError =document.querySelector('#pError');
-    const errorDOM =document.querySelector('#errorDOM');
-    
+    const fragment = document.createDocumentFragment();
+    const mainContainer = document.querySelector('#mainContainer');
+    let busqueda = '';
+    let paginaActual = document.querySelector('#paginaActual');
+    let numeroPagina = paginaActual.value;
+    // const imgCategoria = document.querySelectorAll('.imgCategoria');
+
     // div paginacion
     //botones paginacion
     // boton añadir favoritos 
     //boton eliminar favoritos
     // #favoritos para añidir las imagenes favoritas
-    const fragment = document.createDocumentFragment();
-    const urlBase = `https://api.pexels.com/v1/`;
-    const autorizacion = `851Ebzs3BlLqHHT4VZBNwGS0F7vmu9UH97VyAfhj9mjWBNZ4FRA4zrjt`; // key de Sonia
-    const galeria = document.querySelector('#galeria');
-
-    ////en autorizacion poner su token////
-
-    /*
-const arrayPrueba = [
-    {
-        id: 1,
-        src: 'selva1.jpg',
-        alt: 'selva lorem',
-        p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        id: 2,
-        src: 'selva2.jpg',
-        alt: 'pasarela de madera en el mar',
-        p: 'Consequuntur maxime libero aspernatur sequi quidem deserunt illum praesentium, itaque voluptatibus.',
-    },
-    {
-        id: 1,
-        src: 'selva1.jpg',
-        alt: 'selva lorem',
-        p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        id: 3,
-        src: 'selva3.jpg',
-        alt: 'pasarela de madera en el mar',
-        p: 'Hola Consequuntur munt illum praesentium, itaque voluptatibus.',
-    },
-];
-*/
 
     //EVENTOS
-    //Al pulsar el boton buscar que muestre las imagenes - evento de formulario. pintarImagenes()
-
+    /**
+     * El evento que se activa al pulsar el botón 'buscar' y muestra las imágenes relacionadas con la búsqueda.
+     * @event submit
+     */
     buscadorFotos.addEventListener("submit", (ev) => {
         ev.preventDefault();
-        const palabra = inputBuscador.value.trim().toLowerCase();
-        if (validarInput(palabra)) {
-            pintarImagenes();
+        busqueda = inputBuscador.value.trim().toLowerCase();
+        if (validarInput(busqueda)) {
+            pintarImagenes(busqueda);
         }
     });
 
-    //Al pulsar la orientacion cambiarOrientacion() y  pintarImagenes() ?
-    //Al pulsar cada una de las 3 categorías
-    //Añadir a favoritos  añadirFavoritos()
-    //Eliminar de favoritos
-    //Cambiar de página
+    /**
+     * El evento que se activa al seleccionar la orientación: horizontal, vertical o todo.
+     * @event change
+     */
+    orientacion.addEventListener("change", (ev) => {
+        console.log(ev.target.value);
+        const valorOrientacion = ev.target.value;
+        pintarImagenes(busqueda, valorOrientacion);
+        // return valorOrientacion;
+    });
+
+    /**
+     * El evento que se activa con un click del ratón según el elemento pulsado.
+     * @event click
+     */
+    mainContainer.addEventListener("click", (ev) => {
+        //Al pulsar cada una de las 3 categorías se cargan las fotos de la categoría seleccionada.
+        if (ev.target.matches('.imgCategoria')) {
+            const categoria = ev.target.id;
+            pintarImagenes(categoria, 'horizontal');
+        };
+        //Añadir a favoritos  añadirFavoritos()
+        // console.log(ev.target.matches('.favBtn'));
+        if (ev.target.matches('.favBtn')) {
+            const idFavoritos = ev.target.id;
+            añadirFavoritos(idFavoritos);
+            //!!!! al pintarImagen(hay que también recoger el id de caga imagen 
+            // para usarlo en favoritos e eliminarFavoritos)
+        }
+
+        //Eliminar de favoritos
+        // console.log(ev.target.matches('.elimBtn'));
+        if (ev.target.matches('.elimBtn')) {
+            const idEliminar = ev.target.id;
+            eliminarFavoritos(idEliminar);
+        }
+
+        //Cambiar de página (pendiente: terminar y convertir en una o dos funciones)
+
+        // console.log(ev.target.matches('#paginaAnterior'));
+        if (ev.target.matches('#paginaAnterior')) {
+            console.log(numeroPagina)
+            if (numeroPagina > 1) {
+                numeroPagina--;
+                paginaActual.textContent = numeroPagina;
+                //volver a pintar las imágenes pero ya con una página nueva como parámetro
+            }
+        }
+        // console.log(ev.target.matches('#paginaSiguiente'));
+        if (ev.target.matches('#paginaSiguiente')) {
+            console.log(numeroPagina)
+            if (numeroPagina < 300) {
+                numeroPagina++;
+                paginaActual.textContent = numeroPagina;
+                //volver a pintar las imágenes pero ya con una página nueva como parámetro
+            }
+        }
+    });
+
 
 
     //FUNCIONES
-    // 1- Funcion Validar el texto introducido en el buscador:
-    // Regex: evitar símbolos, números; recortar espacios al principio y al final; pasar todo a minuscula
-    // hay que invocarla dentro del evento "pulsar boton buscar"
-    // REVISAR !!!!!!!! -> intentar hacerlo con un bucle cuando la palabra no es válida
-    const validarInput = (texto) => {
+    /**
+     * Validar el texto introducido en el buscador que solo puede contener palabras y espacios.
+     * @function validarInput
+     * @param {string} busqueda - la palabra que introduce el usuario para el tema de las fotos que se renderizarán.
+     * @returns {boolean} true - confirmación que la palabra ha pasado la validación.
+     */
+    const validarInput = (busqueda) => {
         const regexp = /^[a-záéíóúÁÉÍÓÚüÜñÑ\s]+$/gi;
 
-        if (!regexp.test(texto)) {
+        if (!regexp.test(busqueda)) {
             inputBuscador.classList.add('errorForm');
             pError.textContent = `El texto no es válido. Solo se permiten letras y espacios.`
         } else {
             inputBuscador.classList.remove('errorForm');
             pError.innerHTML = '';
-            return true; 
+            return true;
         }
     }
-    const escribirError = (error) => { 
+
+    const escribirError = (error) => {
         errorDOM.textContent = error;
-        //crear esta clase y editarla en CSS (grande)
         errorDOM.classList.add('errorDOM');
-        
     }
 
-    // 2- Funcion llamar a la API
-    //Variables temporales = para probar si funciona !!!!
-    const url = `search`;
-    let busqueda = `mesa`;
+    const crearUrl = (busqueda, valorOrientacion, numeroPagina) => {
+        let urlCompleta;
+        if (valorOrientacion === 'horizontal') {
+            urlCompleta = `${urlBase}search?query=${busqueda}&size=medium&orientation=landscape&page=${numeroPagina}&per_page=12&locale=es-ES`;
+        } else if (valorOrientacion === 'vertical') {
+            urlCompleta = `${urlBase}search?query=${busqueda}&size=medium&orientation=portrait&page${numeroPagina}&per_page=12&locale=es-ES`;
+        } else {
+            urlCompleta = `${urlBase}search?query=${busqueda}&size=medium&page=${numeroPagina}&per_page=12&locale=es-ES`;
+        }
+        return urlCompleta;
+    }
 
-    const llamarApi = async (url, busqueda) => {
+    /**
+     * Función para llamar a la API de Pexels y obtener las imágenes con el tema solicitado.
+     * @function llamarApi
+     * @param {string} urlCompleta - el enlace completa de la llamada.
+     * @returns {Object} data - objeto con las imágenes y otra información.
+     */
+    const llamarApi = async (urlCompleta) => {
         try {
-            const respuesta = await fetch(`${urlBase}${url}?query=${busqueda}&size=medium&per_page=12&locale=es-ES`, {
+            const respuesta = await fetch(`${urlCompleta}`, {
                 method: 'GET',
                 headers: {
                     Authorization: autorizacion
@@ -113,30 +151,23 @@ const arrayPrueba = [
             const data = await respuesta.json();
             console.log(data);
             return data;
-            //pensar cómo y dónde gestionar este error (mensaje en el DOM)?
         } catch (error) {
             escribirError(error);
-            //console.error(error);
         }
     }
 
-    //quitar de aquí y mover al eventListener o en la función de pintar
-    llamarApi(url, busqueda);
-
-    //Pintamos las fotos debajo de categorías
-
-    // 3- Funcion Pintar imagenes:
-    const pintarImagenes = async () => {
+    //Funcion Pintar imagenes:
+    const pintarImagenes = async (busqueda, valorOrientacion, numeroPagina) => {
         try {
             containerGaleria.innerHTML = ''; // para limpiar imagenes previas - toda la galeria o el container de dentro???
-            const datos = await llamarApi(url, busqueda); // creo que para poder poner 12 fotos tienes que pasar 1º el parametro perPage = 12, en la funcion llamarApi y aquí seria (url, busqueda, 12)
+            const urlCompleta = crearUrl(busqueda, valorOrientacion, numeroPagina);
+            const datos = await llamarApi(urlCompleta);
+
             const fotosTotales = datos.photos;
             //console.log(fotosTotales);
 
             if (!fotosTotales || fotosTotales.length === 0) {  // si no existe, es null o indefined----o el array está vacío
                 throw `No hay imágenes del tema ${busqueda}`;
-                // escribirError('error');
-                // return; // se para la función
             }
             fotosTotales.forEach((foto => {
                 const articleGaleria = document.createElement('ARTICLE');
@@ -166,48 +197,42 @@ const arrayPrueba = [
         }
     }
 
-    pintarImagenes()
 
-    // llamar a la funcion -> validar()
-    //comprobar si no es valido -> hacer un bucle para enviar un mensaje para que introduzca datos validos
-    //Si valido -> llamar API
-    //BUCLE forEach para crear elementos en el DOM
     /*
    Corazón relleno 
    &#9829;   ♥
    Corazón vacío
    &#9825;   ♡
    */
-    // 4- Funcion cambiarOrientacion()
 
-    // 5- Funcion añadirFavoritos()
+    // 5- Funcion añadirFavoritos
     // cuando pulsamos añadir a favoritos -> se guarda en localStorage (con nombre o id de imagen)
     // en localstorage tenemos un array de objetos
     // una funcion getLocalStorage
     // una funcion setLocalStorage
 
-    /*const añadirFavoritos = () => {
-        let favoritos = JSON.parse(localStorage.getItem("favoritos")) || []; 
+    const añadirFavoritos = (id) => {
+        console.log(id)
+        // let favoritos = JSON.parse(localStorage.getItem("favoritos")) || []; 
         // 1º Ver si hay algo en favoritos: localStorage.getItem devuelve string JSON o null si no hay nada 
         // con .parse se convierte en array de objs
         // si es null creamos un array vacio
-        if () {
-
-        }
-        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+        // if () {}
+        // localStorage.setItem("favoritos", JSON.stringify(favoritos));
         //actualiza el array  y lo guarda en localStorage
-    }
-        */
+    };
+
 
     // 6- Funcion pintarFavoritos() dudas: reutilzar pintarImagenes() ??
 
 
     //7- Funcion eliminarFavoritos()
-    //Mirar local storage, buscar la imagen y eliminarla
-    //Volver a pintar
+    const eliminarFavoritos = (id) => {
+        console.log(id)
+        //Mirar local storage, buscar la imagen y eliminarla
+        //Volver a pintar
+    };
 
     // 8- Funcion cambiarPagina()
-
-    //INVOCACIONES
 
 });
